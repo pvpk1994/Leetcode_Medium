cdd59fcf5f52546e9f69f785b45e01d8fe9234cf
Pavan Kumar Paluri <pvpk1994@gmail.com>
2020-09-09 21:42:26 -0500

    Version Comparison between two given versions

---
+++ Python/Version_Compare.py
@@ -0,0 +1,16 @@
+# Leetcode Question: https://leetcode.com/problems/compare-version-numbers/
+# Author: Pavan Kumar Paluri
+# Time Complexity: O(N+M+max(N,M))
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1 = version1.split(".")
+        v2 = version2.split(".")
+        
+        for i in range(max(len(v1), len(v2))):
+            i1 = int(v1[i]) if i < len(v1) else 0
+            i2 = int(v2[i]) if i < len(v2) else 0
+            
+            if i1 != i2:
+                return 1 if i1 > i2 else -1
+        # if here: both are equal
+        return 0
