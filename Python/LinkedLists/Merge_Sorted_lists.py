b9b2405e8cd650773ef823908dba98fb4d63b730
Pavan Kumar Paluri <pvpk1994@gmail.com>
2021-01-04 19:51:31 -0600

    Merge two sorted Linked Lists resulting In a non-decreasing sorted list

---
+++ Python/LinkedLists/Merge_Sorted_lists.py
@@ -0,0 +1,24 @@
+# Merge two sorted linked lists
+# Author: Pavan Kumar Paluri
+# leetcode Question: https://leetcode.com/problems/merge-two-sorted-lists/
+# Time Complexity: O(max(M,N)) {M:len(L1), N:len(L2)}; Space Complexity: O(H) for recursion stack
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None and l2 is None:
+            return None
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
