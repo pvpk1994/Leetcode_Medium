90f9cdd8faf7ec941ab148d5545fb256fd573c50
Pavan Kumar Paluri <pvpk1994@gmail.com>
2021-01-04 20:05:37 -0600

    Iterative approach added

--- Python/LinkedLists/Merge_Sorted_lists.py
+++ Python/LinkedLists/Merge_Sorted_lists.py
@@ -22,3 +22,26 @@ class Solution:
         else:
             l2.next = self.mergeTwoLists(l1, l2.next)
             return l2
+
+# Iterative Approach
+# Time Complexity O(Max(M,N)) and Space complexity: O(1) - Constant Space
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        # maintain an unchanging reference to node ahead of the return node.
+        prehead = ListNode(-1)
+
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next            
+            prev = prev.next
+
+        # exactly one of l1 and l2 can be non-null at this point, so connect
+        # the non-null list to the end of the merged list.
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next
